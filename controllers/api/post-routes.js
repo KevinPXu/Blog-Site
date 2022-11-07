dbf13f021f1cce4679271fe23cf7af070a0f9eb9
Kevin Xu <kevinpxu21@gmail.com>
2022-11-06 18:38:38 -0600

    added user model

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const { Post } = require("../../models/posts");
+
+router.post("/", async (req, res) => {
+  try {
+    const postData = await Post.create({
+      title: req.body.title,
+      text: req.body.text,
+    });
+
+    res.status(200).json(postData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
