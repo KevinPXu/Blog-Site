01974b813e6ef7cf9d7b969a4f9c2cd670a07744
Kevin Xu <kevinpxu21@gmail.com>
2022-11-08 15:17:25 -0600

    added login js to public folder

---
+++ public/js/login.js
@@ -0,0 +1,49 @@
+const loginFormHandler = async (e) => {
+  e.preventDefault();
+
+  const username = document.querySelector("#username-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (username && password) {
+    const res = await fetch("/api/user/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (res.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert(res.statusText);
+    }
+  }
+};
+
+const signupFormHandler = async (e) => {
+  e.preventDefault();
+
+  const username = document.querySelector("#username-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  if (username && password) {
+    const res = await fetch("/api/user", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (res.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert(res.statusText);
+    }
+  }
+};
+
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
