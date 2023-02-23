2b927611327f68413d21fbe831f19644856f3908
Kevin Xu <kevinpxu21@gmail.com>
2023-02-23 14:49:42 -0600

    added a endpoint to view the comments for a single post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,15 +1,15 @@
-const router = require("express").Router();
-const { Post, User, Comment } = require("../models");
-const auth = require("../utils/authenticate");
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+const auth = require('../utils/authenticate');
 
 //get route for the home page which renders all posts that are already on the page and by which user
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const postData = await Post.findAll({
       include: [
         {
           model: User,
-          attributes: ["username"],
+          attributes: ['username'],
         },
       ],
     });
@@ -20,15 +20,15 @@ router.get("/", async (req, res) => {
 
     res
       .status(200)
-      .render("homepage", { posts, loggedIn: req.session.logged_in });
+      .render('homepage', { posts, loggedIn: req.session.logged_in });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
 
-// a get route that brings the user to their own dashboard which will load all posts they have created 
-router.get("/dashboard", auth, async (req, res) => {
+//get route that brings the user to their own dashboard which will load all posts they have created
+router.get('/dashboard', auth, async (req, res) => {
   try {
     const postData = await Post.findAll({
       where: {
@@ -37,7 +37,7 @@ router.get("/dashboard", auth, async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ["username"],
+          attributes: ['username'],
         },
       ],
     });
@@ -48,17 +48,17 @@ router.get("/dashboard", auth, async (req, res) => {
 
     res
       .status(200)
-      .render("dashboard", { posts, loggedIn: req.session.logged_in });
+      .render('dashboard', { posts, loggedIn: req.session.logged_in });
   } catch (err) {
     console.error(err);
     res.status(500).json(err);
   }
 });
 
-//get rout that allows the user to create a new post by rendering a new post form
-router.get("/dashboard/create", auth, async (req, res) => {
+//get route that allows the user to create a new post by rendering a new post form
+router.get('/dashboard/create', auth, async (req, res) => {
   try {
-    res.status(200).render("newPost", { loggedIn: req.session.logged_in });
+    res.status(200).render('newPost', { loggedIn: req.session.logged_in });
   } catch (err) {
     console.error(err);
     res.status(500).json(err);
@@ -66,19 +66,38 @@ router.get("/dashboard/create", auth, async (req, res) => {
 });
 
 //get route that allows the user to edit an existing post they created by rendering a new edit form
-router.get("/dashboard/edit/:id", auth, async (req, res) => {
+router.get('/dashboard/edit/:id', auth, async (req, res) => {
   try {
     const postData = await Post.findOne({
       where: {
         id: req.params.id,
       },
+      include: Comment,
     });
 
     //obtains the raw formatted JSON from the sequelize methods
     const post = postData.get({ plain: true });
     res
       .status(200)
-      .render("editPost", { post, loggedIn: req.session.logged_in });
+      .render('editPost', { post, loggedIn: req.session.logged_in });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/dashboard/view/:id', auth, async (req, res) => {
+  try {
+    const commentData = await Post.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    const comment = commentData.get({ plain: true });
+    res
+      .status(200)
+      .render('viewPost', { comment, loggedIn: req.session.logged_in });
   } catch (err) {
     console.error(err);
     res.status(500).json(err);
@@ -86,10 +105,10 @@ router.get("/dashboard/edit/:id", auth, async (req, res) => {
 });
 
 //gets a post by their own id as well as all the comments associated with it
-router.get("/post/:id", auth, async (req, res) => {
+router.get('/post/:id', auth, async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id, {
-      include: [{ model: User, attributes: ["username"] }],
+      include: [{ model: User, attributes: ['username'] }],
     });
 
     const commentData = await Comment.findAll({
@@ -99,7 +118,7 @@ router.get("/post/:id", auth, async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ["username"],
+          attributes: ['username'],
         },
       ],
     });
@@ -110,7 +129,7 @@ router.get("/post/:id", auth, async (req, res) => {
 
     res
       .status(200)
-      .render("comment", { post, comment, loggedIn: req.session.logged_in });
+      .render('comment', { post, comment, loggedIn: req.session.logged_in });
   } catch (err) {
     console.error(err);
     res.status(500).json(err);
@@ -118,8 +137,8 @@ router.get("/post/:id", auth, async (req, res) => {
 });
 
 //gets the login form and renders it to the page
-router.get("/login", (req, res) => {
-  res.render("login");
+router.get('/login', (req, res) => {
+  res.render('login');
 });
 
 module.exports = router;
